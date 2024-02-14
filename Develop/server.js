838adcfa77f9731fd9f08ebe63409f9d62076913
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-14 20:38:49 +1100

    required necessary libraries in server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,13 @@
 // Loading required libaries
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const api = require('./routes/index.js');
 
+// Port to listen on
 const PORT = process.env.port || 3001;
 
+// Instance of Application with Express()
 const app = express();
 
 
@@ -11,7 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', api);
 
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 
