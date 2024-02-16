6a3b5e8d2e22fed645b8e84fccf237b3927d873c
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-16 19:02:24 +1100

    Added routes to api_routes, html_routes and server.js

--- Develop/server.js
+++ Develop/server.js
@@ -10,11 +10,22 @@ const PORT = process.env.port || 3001;
 // Instance of Application with Express()
 const app = express();
 
+// Routes 
+const htmlRoute = require('./routes/html_routes');
+const apiRoute = require('./routes/api_route');
+app.use('/html_routes', htmlRoute);
+app.use('/api_routes', apiRouteRoute);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
+
+
+
+// Send all the req's the start with '/api' to the index.js file in the routes folder
 app.use('/api', api);
 
 // GET Route for homepage
@@ -22,7 +33,10 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
+// GET Route for notes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 
