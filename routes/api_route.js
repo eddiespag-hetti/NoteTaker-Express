72045c3b586a99db51b60cf7e6080cccd309828c
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-19 21:31:40 +1100

    Added and installed 'uuid' library

--- routes/api_route.js
+++ routes/api_route.js
@@ -3,6 +3,10 @@ const express = require("express");
 const api_route = express.Router();
 const fs = require("fs");
 
+// v4 uuid is grabbed through installing "npm i uuid".
+const { v4: uuidv4 } = require('uuid');
+
+
 // Get Request route handler for data read from json file
 api_route.get("/", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (error, data) =>
@@ -15,7 +19,15 @@ api_route.post("/", (req, res) => {
   console.log(req.body);
   fs.readFile("./db/db.json", "utf8", (error, data) => {
     const oldNotes = JSON.parse(data);
-    oldNotes.push(req.body);
+
+    const newInput = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+  };
+
+
+    oldNotes.push(newInput);
     fs.writeFile("./db/db.json", JSON.stringify(oldNotes), (err) =>
       err
         ? console.error(err)
